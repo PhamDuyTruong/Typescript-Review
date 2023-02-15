0af2caecfe63587740601ef4b3cf5919701a8c7d
20120230 <duytruong5437@gmail.com>
2023-02-15 14:11:26 +0700

    create post

--- bai02/client/src/pages/create-post.tsx
+++ bai02/client/src/pages/create-post.tsx
@@ -1,15 +1,43 @@
 import { Box, Button, Flex, FormControl, Spinner } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
+import router from 'next/router';
 import React from 'react'
 import InputField from '../components/InputField';
 import Layout from '../components/Layout';
+import { CreatePostInput, useCreatePostMutation } from '../generated/graphql';
 import { useCheckAuth } from '../utils/useCheckAuth';
 
 const CreatePost = () => {
     const { data: authData, loading: authLoading } = useCheckAuth();
     const initialValues = {title: '', text: ''}
-    const handleCreateSubmit = () => {
+    const [createPost, _] = useCreatePostMutation();
+    const handleCreateSubmit = async (values: CreatePostInput) => {
+        await createPost({
+            variables: {createPostInput: values},
+            update(cache, {data}){
+                cache.modify({
+                    fields: {
+                        posts(existing){
+                            if(data?.createPost.success && data.createPost.post){
+                                const newPostRef = cache.identify(data.createPost.post);
 
+                                const newPostAfterCreation = {
+                                    ...existing,
+                                    totalCount: existing.totalCount + 1,
+                                    paginatedPosts: [
+                                        {_ref: newPostRef},
+                                        ...existing.paginatedPost,
+                                        
+                                    ]
+                                }
+                                return newPostAfterCreation
+                            }
+                        }
+                    }
+                })
+            }
+        })
+        router.push("/")
     }
     if (authLoading || (!authLoading && !authData?.me)) {
         return (
