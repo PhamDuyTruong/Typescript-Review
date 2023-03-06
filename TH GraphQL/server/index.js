3f124168f6177cc90688159e9f2e8acec552e994
20120230 <duytruong5437@gmail.com>
2023-03-06 22:01:04 +0700

    add model in mongoDB

--- TH GraphQL/server/index.js
+++ TH GraphQL/server/index.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const {ApolloServer} = require("apollo-server-express");
-
+const mongoose = require("mongoose");
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolver/resolver");
+require("dotenv").config();
 
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
-});
+const connectToDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1);
+    }
+}
 
+connectToDB();
 const app = express();
-server.applyMiddleware({app});
 
-app.listen({port: 4000}, () => {
-    console.log(`Server run at http://localhost:4000${server.graphqlPath}`);
-})
+async function startServer() {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen({port: 4000}, () => {
+        console.log(`Server run at http://localhost:4000${apolloServer.graphqlPath}`);
+    })
+};
+
+startServer();
+
+
