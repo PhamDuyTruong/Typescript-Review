b1526dffa8196f7db9d52c78784dd10e7e7a30e4
20120230 <duytruong5437@gmail.com>
2023-03-08 17:05:42 +0700

    add book

--- TH GraphQL/client/src/components/Forms.js
+++ TH GraphQL/client/src/components/Forms.js
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import {useQuery} from '@apollo/client';
-import {getAuthors} from '../graphql-client/queries'
+import {useQuery, useMutation} from '@apollo/client';
+import {getAuthors} from '../graphql-client/queries';
+import {addBook} from '../graphql-client/mutations'
 
 const Forms = () => {
+    const [newBook, setNewBook] = useState({
+        name: '',
+        genre: '',
+        authorId: ''
+    });
+
+    const handleInputChange = e => {
+        setNewBook({
+            ...newBook,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Query
     const {loading, error, data} = useQuery(getAuthors);
+
+    //Mutation
+    const [createBook, dataMutation] = useMutation(addBook);
+
+
+
+    const handleSubmitBook = (e) => {
+        e.preventDefault();
+        createBook({
+            variables: {
+                name: newBook.name,
+                genre: newBook.genre,
+                authorId: newBook.authorId
+            }
+        });
+        setNewBook({
+            name: '',
+            genre: '',
+            authorId: ''
+        })
+    }
 
   return (
     <Row>
         <Col>
-            <Form>
+            <Form onSubmit={handleSubmitBook}>
                 <Form.Group>
-                    <Form.Control type='text' placeholder='Book name'/>
+                    <Form.Control type='text' placeholder='Book name' name='name' onChange={handleInputChange} value={newBook.name}/>
                 </Form.Group>
                 <Form.Group>
-                    <Form.Control type='text' placeholder='Book genre'/>
+                    <Form.Control type='text' placeholder='Book genre' name='genre'onChange={handleInputChange} value={newBook.genre}/>
                 </Form.Group>
                 <Form.Group>
                     {loading ? <p>Loading...</p>: null}
-                    <Form.Control as='select' defaultValue="Select Author">
+                    <Form.Control as='select' defaultValue="Select Author" name='authorId' onChange={handleInputChange} value={newBook.authorId}>
                         <option disabled>Select Author</option>
                         {data.authors.map(item => (
                             <option key={item.id} value={item.id}>
@@ -30,7 +66,7 @@ const Forms = () => {
                         ))}
                     </Form.Control>
                 </Form.Group>
-                <Button className='float-right' variant='info' type='submit'>Add Book</Button>
+                <Button className='float-right' variant='info' type='submit' >Add Book</Button>
             </Form>
         </Col>
 
