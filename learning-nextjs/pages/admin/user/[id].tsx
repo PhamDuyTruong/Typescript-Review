00af1f29704fb6a3dd540e54f8e3bd14db9d4cc2
20120230 <duytruong5437@gmail.com>
2023-03-19 20:33:30 +0700

    học về dynamic route và static file serve

---
+++ learning-nextjs/pages/admin/user/[id].tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head'
+
+const UserPage = () => {
+  return (
+    <>
+        <Head>
+           <title>User detail</title>
+        </Head>
+       <h1>Admin - user page</h1>
+    </>
+   
+  )
+}
+
+export default UserPage
