4506bc80d775af578d21552689169de8c2a243a0
20120230 <duytruong5437@gmail.com>
2023-03-20 19:51:35 +0700

    set up project

---
+++ Learning Postgres/backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config()
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8001"
+  };
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Scheduler back-end service." });
+  });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
